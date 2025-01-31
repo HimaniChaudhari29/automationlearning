576c56cc06f476bf621fedd8e145eacbfafca590
Himani Chaudhari <himanichaudhari29@gmail.com>
2025-01-31 12:20:28 +0530

    Increase array list

--- ReverseArray.java
+++ ReverseArray.java
@@ -4,11 +4,11 @@ public class ReverseArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            int[] arr= {2,4,6,3,1};
+            int[] arr= {2,4,6,3,1,7};
             int s=arr.length;
             int revarr=0;
          
-            for(int i=0;i<5;i++)
+            for(int i=0;i<6;i++)
             {
             	revarr=arr[i];
             	
