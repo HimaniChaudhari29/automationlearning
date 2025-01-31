b3620a5c73c4e6b1c38bb8a0f543cc3f12923653
Himani Chaudhari <himanichaudhari29@gmail.com>
2025-01-31 12:45:10 +0530

    All file are done with changes

---
+++ SumofArrayEle.java
@@ -0,0 +1,29 @@
+package assignment.array;
+
+import java.util.Scanner;
+//class name change
+public class SumofArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int sum = 0;
+
+		int[] arr = new int[5];
+		System.out.println("Enter the elements in array :");
+		for (int a : arr) {
+
+			a = sc.nextInt();
+			System.out.println(a);
+
+			sum = sum + a;
+
+		}
+
+		System.out.println("Sum of array elements " +sum);
+
+	}
+
+}
---
+++ TestYourSetup.java
@@ -0,0 +1,38 @@
+package scripts;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestYourSetup {
+
+	WebDriver driver;
+
+	@Test
+	public void loginToNicheThyselfTourism() {
+		driver.get("https://nichethyself.com/tourism/home.html");
+		WebElement username = driver.findElement(By.name("username"));
+		username.sendKeys("stc123");
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("12345");
+		username.submit();
+		String ExpectedPageTitleAfterLogin="My account";
+		String ActualPageTitleAfterLogin=driver.getTitle();
+		  assertEquals(ActualPageTitleAfterLogin,ExpectedPageTitleAfterLogin);
+	}
+
+	@BeforeMethod()
+	public void beforeMethod() {
+		driver = new ChromeDriver();
+		// driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+
+	}
+
+}
